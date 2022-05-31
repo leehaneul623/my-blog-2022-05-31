4cbaf14cc789074437124a79b945f809af5cf939
leehaneul623 <leehaneul623@github.com>
2022-06-01 08:57:14 +0900

    테스트 페이지 추가

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "gatsby";
 
 function TestPage() {
   return (
     <main>
       <title>테스트</title>
       <h1>테스트 페이지 입니다.</h1>
+      <Link to="/test">Test</Link>
+      <p>I'm making this by following the Gatsby Tutorial.</p>
     </main>
   );
 }
