0b04618720449f3a970f5b6173bd0ea9fdd4b7d2
leehaneul623 <dyj03216@naver.com>
2022-06-01 22:11:01 +0900

    그래프QL로 데이터 가져와서 레이아웃에 표시2

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteMetadata: {
       title: "My First Gatsby Site",
+      description: "이 사이트에 개발자 이하늘의 개발일지가 담겨 있습니다."
   },
   plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
 };
--- src/components/layout.js
+++ src/components/layout.js
@@ -10,12 +10,24 @@ import {
 } from "./layout.module.css";
 
 function Layout({ pageTitle, children }) {
+  const data = useStaticQuery(graphql`
+    query{
+      site{
+        siteMetadata{
+          title
+          description
+        }
+      }
+    }
+  `);
 
-  
   return (
-
     <div className={container}>
-      <title>{pageTitle}</title>
+      <title>
+        {pageTitle} | {data.site.siteMetadata.title}
+      </title>
+      <header>{data.site.siteMetadata.title}</header>
+      <div>{data.site.siteMetadata.description}</div>
       <nav>
         <ul className={navLinks}>
           <li className={navLinkItem}>
