4f5f99b13e6bb42a1761c823ecd5d16fd6bde8ef
leehaneul623 <dyj03216@naver.com>
2022-06-02 17:16:33 +0900

    GatsbyImage 컴포넌트로 동적 이미지 노출

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,12 +1,24 @@
 import React from "react";
 import { graphql } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import Layout from "../../components/layout";
 const BlogPost = ({ data }) => {
+  const image = getImage(data.mdx.frontmatter.hero_image);
+
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
       <p>{data.mdx.frontmatter.date}</p>
-      <div>{data.mdx.frontmatter.hero_image.name}</div>
+      <GatsbyImage
+      image={image}
+      alt={data.mdx.frontmatter.hero_image_alt}
+    />
+          <p>
+        Photo Credit:{" "}
+        <a target="_blank"a href={data.mdx.frontmatter.hero_image_credit_link}>
+          {data.mdx.frontmatter.hero_image_credit_text}
+        </a>
+      </p>
       <hr />
       <MDXRenderer>{data.mdx.body}</MDXRenderer>
     </Layout>
@@ -17,14 +29,16 @@ export const query = graphql`
     mdx(id: { eq: $id }) {
       frontmatter {
         title
-        date(formatString: "MMMM D, YYYY")
+        date(formatString: "MMMM DD, YYYY")
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
         hero_image {
-          relativePath
-          name
-          ext
-          size
-        }
-      }
+          childImageSharp {
+            gatsbyImageData
+           }
+          }
+         }
       body
     }
   }
