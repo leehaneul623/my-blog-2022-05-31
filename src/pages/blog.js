e563b3ad85a23a881e8c2900f1a37a6751735d33
leehaneul623 <dyj03216@naver.com>
2022-06-02 09:39:49 +0900

    블로그 포스트리스트에서 내용 표시

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,29 +1,33 @@
 import { graphql, useStaticQuery } from "gatsby";
 import React from "react";
+import { MDXRenderer } from "gatsby-plugin-mdx";
 import Layout from "../components/layout";
 
 function BlogPage() {
   const data = useStaticQuery(graphql`
-    query{
-        allFile {
-          nodes {
-            name
+    query {
+      allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+        nodes {
+          frontmatter {
+            date(formatString: "MMMM D, YYYY")
+            title
           }
+          id
+          body
         }
       }
+    }
   `);
 
   return (
     <Layout pageTitle="My Blog Posts">
-      <p>
-      My cool posts will go in here
-      </p>
-
-      <ul>
-        {data.allFile.nodes.map((node) => (
-          <li key={node.name}>{node.name}</li>
-        ))}
-      </ul>
+      {data.allMdx.nodes.map((node) => (
+        <article key={node.id}>
+          <h2>{node.frontmatter.title}</h2>
+          <p>Posted: {node.frontmatter.date}</p>
+          <MDXRenderer>{node.body}</MDXRenderer>
+        </article>
+      ))}
     </Layout>
   );
 }
